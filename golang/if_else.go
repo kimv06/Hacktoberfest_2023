81370ba1874aa255f3c0d58d69617d9194d2ea60
mistershashanksingh <mistershashanksingh@gmail.com>
2023-10-20 20:51:35 +0530

    add if_else in go(golang)

---
+++ golang/if_else.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := 100
+
+	if x == 50 {
+		fmt.Println("Option-1")
+	} else if x == 100 {
+		fmt.Println("Option-2")
+	} else {
+		fmt.Println("No_Option")
+	}
+}
